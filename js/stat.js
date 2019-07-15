76ff809a014107dcb7ea473345a26317dc2c7411
Alex <lexashkel@mail.ru>
2019-07-15 19:59:52 +0300

    еще 2е задание по 20 скрин

--- js/stat.js
+++ js/stat.js
@@ -2,6 +2,13 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 15;
+var TEXT_WIDTH = 50;
+var BAR_HEIGHT = 20;
+var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
 
 var renderCloud = function(ctx, x, y, color) {
     ctx.fillStyle = color;
@@ -9,19 +16,19 @@ var renderCloud = function(ctx, x, y, color) {
 };
 
 window.renderStatistics = function(ctx) {
-    renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
-    renderCloud(ctx, 100, 10, '#fff');
+    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
     ctx.fillStyle = '#000';
-    ctx.fillText('Вы', 110, 35);
-    ctx.fillRect(110, 40, 400, 20);
+    ctx.fillText('Вы', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP);
+    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP, barWidth, BAR_HEIGHT);
 
     ctx.fillStyle = '#000';
-    ctx.fillText('Иван', 110, 75);
+    ctx.fillText('Иван', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + BAR_HEIGHT + GAP);
     ctx.fillRect(110, 80, 400, 20);
 
     ctx.fillStyle = '#000';
-    ctx.fillText('Бишоп', 110, 115);
+    ctx.fillText('Бишоп', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (BAR_HEIGHT + GAP) * 2);
     ctx.fillRect(110, 120, 370, 20);
 };
 
