756cfc3f1cfcccf986620751838a5749e84d5a51
Alex <lexashkel@mal.ru>
2019-07-15 17:26:16 +0300

    часть второго задания

--- js/stat.js
+++ js/stat.js
@@ -1,9 +1,27 @@
 'use strict'
 
-window.renderStatistics = function(ctx) {
-    ctx.fillStyle('fff');
-    ctx.fillRect(100, 10, 420, 270);
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 
-    ctx.fillStyle('rgba(0, 0, 0, 0.7)');
-    ctx.fillRect(110, 20, 420, 270);
+var renderCloud = function(ctx, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
+
+window.renderStatistics = function(ctx) {
+    renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+    renderCloud(ctx, 100, 10, '#fff');
+
+    ctx.fillStyle = '#000';
+    ctx.fillText('Вы', 110, 35);
+    ctx.fillRect(110, 40, 400, 20);
+
+    ctx.fillStyle = '#000';
+    ctx.fillText('Иван', 110, 75);
+    ctx.fillRect(110, 80, 400, 20);
+
+    ctx.fillStyle = '#000';
+    ctx.fillText('Бишоп', 110, 115);
+    ctx.fillRect(110, 120, 370, 20);
+};
+
