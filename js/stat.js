78a43d17443ca80b50de1b3b0de9dfcdb932f999
Alex <lexashkel@mal.ru>
2019-07-16 17:26:18 +0300

    почти 2 задание

--- js/stat.js
+++ js/stat.js
@@ -7,28 +7,43 @@ var CLOUD_Y = 10;
 var GAP = 10;
 var FONT_GAP = 15;
 var TEXT_WIDTH = 50;
-var BAR_HEIGHT = 20;
+var BAR_WIDTH = 40;
+var BAR_RANGE = 50;
 var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+var HISTOGRAM_HEIGHT = 150;
 
 var renderCloud = function(ctx, x, y, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function(ctx) {
+var getMaxElement = function(arr) {
+    var maxElement = arr[0];
+    
+    for (var i = 1; i < arr.length; i ++) {
+        if (arr[i] > maxElement && arr.length > 0) {
+          maxElement = arr[i];
+        }
+    }
+
+    return maxElement;
+};
+
+window.renderStatistics = function(ctx, names, times) {
     renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
     renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
     ctx.fillStyle = '#000';
-    ctx.fillText('Вы', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP);
-    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP, barWidth, BAR_HEIGHT);
 
-    ctx.fillStyle = '#000';
-    ctx.fillText('Иван', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + BAR_HEIGHT + GAP);
-    ctx.fillRect(110, 80, 400, 20);
+    ctx.font = '16px PT Mono';
+    ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP);
+    ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + GAP + FONT_GAP);
 
-    ctx.fillStyle = '#000';
-    ctx.fillText('Бишоп', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (BAR_HEIGHT + GAP) * 2);
-    ctx.fillRect(110, 120, 370, 20);
+    var maxTime = getMaxElement(times);
+
+    // //for (var i = 0; i < names.length; i++) {
+    //     ctx.fillText(names[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (BAR_HEIGHT + GAP) * i);
+    //     ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (BAR_HEIGHT + GAP) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+    // }
 };
 
