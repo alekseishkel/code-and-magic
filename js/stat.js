506d45439f805a5a316eadfe199f70e2c2cfc66b
Alex <lexashkel@mal.ru>
2019-07-17 17:27:54 +0300

    2 задание без цвета гистограммы

--- js/stat.js
+++ js/stat.js
@@ -6,10 +6,9 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
 var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
+var TEXT_HEIGHT = 20;
 var BAR_WIDTH = 40;
 var BAR_RANGE = 50;
-var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
 var HISTOGRAM_HEIGHT = 150;
 
 var renderCloud = function(ctx, x, y, color) {
@@ -41,9 +40,19 @@ window.renderStatistics = function(ctx, names, times) {
 
     var maxTime = getMaxElement(times);
 
-    // //for (var i = 0; i < names.length; i++) {
-    //     ctx.fillText(names[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (BAR_HEIGHT + GAP) * i);
-    //     ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (BAR_HEIGHT + GAP) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
-    // }
+    var colors = ['0.3', '0.4', '0.5', '0.6', '0.7'];
+
+    var myDiv = colors[Math.floor(Math.random() * colors.length)];
+    console.log(myDiv);
+
+    ctx.fillStyle = 'rgba(255, 255, 0, 1)';
+
+    for (var i = 0; i < names.length; i++) {
+      ctx.fillText(names[i], CLOUD_X + BAR_RANGE + (BAR_WIDTH + BAR_RANGE) * i, CLOUD_HEIGHT);
+      ctx.transform(1, 0, 0, -1, 0, 270);
+      
+      ctx.fillRect(CLOUD_X + BAR_RANGE + (BAR_WIDTH + BAR_RANGE) * i, TEXT_HEIGHT, BAR_WIDTH, (HISTOGRAM_HEIGHT * times[i]) / maxTime);
+      ctx.transform(1, 0, 0, -1, 0, 270);
+    };
 };
 
