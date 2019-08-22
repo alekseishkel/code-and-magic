050ce68d2d7c38e6d267b196518e546b1d5392a9
Alex <lexashkel@mal.ru>
2019-08-22 17:27:06 +0300

    7 задание 15 слайд

---
+++ js/render.js
@@ -0,0 +1,31 @@
+'use strict';
+
+(function () {
+
+  var similarWizardTemplate = document.getElementById('similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+
+    return wizardElement;
+  };
+
+  var similarListElement = document.querySelector('.setup-similar-list');
+
+  window.render = function (data) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < 4; i++) {
+      fragment.appendChild(renderWizard(data[i]));
+    }
+
+    similarListElement.appendChild(fragment);
+  };
+
+})();
--- js/similar.js
+++ js/similar.js
@@ -1,106 +1,70 @@
-'use strict'
+'use strict';
 
-var similarListElement = document.querySelector('.setup-similar-list');
-
-var similarWizardTemplate = document.getElementById('similar-wizard-template')
-  .content
-  .querySelector('.setup-similar-item');
-
-var renderWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
-
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
-
-  return wizardElement;
-};
-
-var updateWizards = function (wizards) {
-  // var mostSimilarWizards = wizards.filter(function (it) {
-  //   return it.colorCoat === window.coatColor && it.colorEyes === window.eyesColor &&
-  //     it.colorFireball === window.fireballColor;
-  // });
-
-  // var sameCoatWizards = wizards.filter(function (it) {
-  //   return it.colorCoat === window.coatColor;
-  // });
-  // var sameEyesWizards = wizards.filter(function (it) {
-  //   return it.colorEyes === window.eyesColor;
-  // });
-  // var sameFireballWizards = wizards.filter(function (it) {
-  //   return it.colorFireball === window.fireballColor;
-  // });
-
-  // var filteredWizards = mostSimilarWizards;
-  // filteredWizards = mostSimilarWizards.concat(sameCoatWizards);
-  // filteredWizards = mostSimilarWizards.concat(sameEyesWizards);
-  // filteredWizards = mostSimilarWizards.concat(sameFireballWizards);
-  // filteredWizards = mostSimilarWizards.concat(wizards);
-
-  // var uniqueWizards = filteredWizards.filter(function (it, i) {
-  //   return filteredWizards.indexOf(it) === i;
-  // });
-
-  renderWizard(wizards.sort(function (left, right) {
-    var rankDiff = getRank(right) - getRank(left);
-    if (rankDiff === 0) {
-      rankDiff = wizardsSort(left.name, right.name);
+(function () {
+  var getRank = function (wizard) {
+    var rank = 0;
+    if (wizard.colorCoat === window.coatColor) {
+      rank += 2;
     }
-    return rankDiff;
-  }));
-};
+    if (wizard.colorEyes === window.eyesColor) {
+      rank += 1;
+    }
+    if (wizard.colorFireball === window.fireballColor) {
+      rank += 1;
+    }
 
+    return rank;
+  };
 
-var getRank = function (wizard) {
-  var rank = 0;
-  if (wizard.colorCoat === window.coatColor) {
-    rank += 2;
-  }
-  if (wizard.colorEyes === window.eyesColor) {
-    rank += 1;
-  }
-  if (wizard.colorFireball === window.fireballColor) {
-    rank +=1;
-  }
-
-  return rank;
-};
-
-var wizardsSort = function(left, right) {
-  if (left > right) {
-    return 1;
-  } else if (left < right) {
-    return -1;
-  } else {
-    return 0;
-  }
-};
-
-var wizards = [];
-
-var loadHandler = (function (data) {
-  wizards = data;
-
-  var fragment = document.createDocumentFragment();
-
-  for (var i = 0; i < 4; i++) {
-    fragment.appendChild(renderWizard(wizards[i]));
+  var namesRank = function (left, right) {
+    if (left > right) {
+      return 1;
+    } else if (left < right) {
+      return -1
+    } else {
+      return 0;
+    }
   }
 
-  similarListElement.appendChild(fragment);
-});
+  var updateWizards = function () {
+    window.render(wizards.sort(function (left, right) {
+      var rankDiff = getRank(right) - getRank(left);
+      if (rankDiff === 0) {
+        rankDiff = namesRank(left.name, right.name);
+      }
+      return rankDiff;
+    }));
+  }
 
-window.errorHandler = (function (errorMessage) {
-  var node = document.createElement('div');
-  node.style = 'z-index: 100; text-align: center; background-color: red;';
-  node.style.position = 'absolute';
-  node.style.left = 0;
-  node.style.right = 0;
-  node.style.fontSize = '30px';
+  window.wizard.onCoatChange = function (color) {
+    window.coatColor = color;
+    updateWizards();
+  };
+  window.wizard.onEyesChange = function (color) {
+    window.eyesColor = color;
+    updateWizards();
+  };
 
-  node.textContent = errorMessage;
-  document.body.insertAdjacentElement('afterbegin', node);
-});
+  var wizards = [];
 
-window.load(loadHandler, window.errorHandler);
+
+  var loadHandler = function (data) {
+    wizards = data;
+    updateWizards();
+  };
+
+  window.errorHandler = (function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  });
+
+  window.load(loadHandler, window.errorHandler);
+
+})();
