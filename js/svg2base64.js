77b27ae28c6e04924a15b7bd59bba6885190c70d
Alex <lexashkel@mail.ru>
2019-08-30 17:04:07 +0300

    9 задание корректировка

--- js/svg2base64.js
+++ js/svg2base64.js
@@ -5,12 +5,8 @@
 
  window.svg2base64 = function (svgElement) {
     var xml = new XMLSerializer().serializeToString(svgElement);
-    console.log("1" + xml);
 
     var svg64 = this.window.btoa(xml);
-    console.log("2 " + svg64);
-
-    console.log("3 " + DATA_URI_PREFIX + svg64);
 
     return DATA_URI_PREFIX + svg64;
  }
